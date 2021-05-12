27d2d9d413e29638af5f31575d49ae6963c6f95e
btxuyenhcmus <btxuyenhcmus@gmail.com>
2021-05-13 00:53:16 +0700

    [ADD] btxuyen finish step two, edit report and continue code sequence

--- src/visualization_utils.py
+++ src/visualization_utils.py
@@ -1,4 +1,7 @@
-from PIL import ImageDraw
+from PIL import ImageDraw, Image
+from src.box_utils import correct_bboxes
+import numpy as np
+import math
 
 
 def show_bboxes(img, bounding_boxes, facial_landmarks=[]):
@@ -27,5 +30,30 @@ def show_bboxes(img, bounding_boxes, facial_landmarks=[]):
                 (p[i] - 1.0, p[i + 5] - 1.0),
                 (p[i] + 1.0, p[i + 5] + 1.0)
             ], outline='blue')
-
     return img_copy
+
+def show_image_resize(image, scale):
+    width, height = image.size
+    sw, sh = math.ceil(width*scale), math.ceil(height*scale)
+    img = image.resize((sw, sh), Image.BILINEAR)
+    return img
+
+def show_image_resize_bounding_box(bounding_boxes, img, size=24):
+    num_boxes = len(bounding_boxes)
+    width, height = img.size
+
+    [dy, edy, dx, edx, y, ey, x, ex, w, h] = correct_bboxes(bounding_boxes, width, height)
+    img_boxes = []
+
+    for i in range(num_boxes):
+        img_box = np.zeros((h[i], w[i], 3), 'uint8')
+
+        img_array = np.asarray(img, 'uint8')
+        img_box[dy[i]:(edy[i] + 1), dx[i]:(edx[i] + 1), :] =\
+            img_array[y[i]:(ey[i] + 1), x[i]:(ex[i] + 1), :]
+
+        # resize
+        img_box = Image.fromarray(img_box)
+        img_box = img_box.resize((size, size), Image.BILINEAR)
+        img_boxes.append(img_box)
+    return img_boxes
