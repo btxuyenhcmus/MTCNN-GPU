76af7c9ef351cd88574048a8f62df0ab1769979d
btxuyenhcmus <btxuyenhcmus@gmail.com>
2021-06-09 23:54:08 +0700

    [ADD] add torch jit script for load net

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src import detect_faces, show_bboxes
+from src import detect_faces, show_bboxes, detect_faces_with_jit
 from PIL import Image
 img = Image.open('images/family.jpeg')
 bounding_boxes, landmarks = detect_faces(img)
