e21b9c51b7f4b135c24f12b35913c68089f00fed
btxuyenhcmus <btxuyenhcmus@gmail.com>
2021-06-21 22:19:05 +0700

    [ADD] cuda.jit_functional: apply cuda.jit to prelu function

--- torch/nn/jit_functional.py
+++ torch/nn/jit_functional.py
@@ -17,6 +17,20 @@ def conv_forward_type1(input, weight, bias, stride, padding, dilation, groups):
     _conv_forward_type1(np_input_pad,np_weight, np_bias, stride, result)
     return torch.from_numpy(result).type_as(input)
 
+def conv_forward_type2(input, weight, bias, stride, padding, dilation, groups):
+    np_input_pad = np.pad(input.data.cpu().numpy(), pad_width=((0,), (0,), (padding[0],), (padding[1],)), mode='constant', constant_values=0)
+    np_weight = weight.data.cpu().numpy()
+    np_bias = bias.data.cpu().numpy()
+    
+    # Dimensions.
+    N, Kc, H, W = input.shape
+    C, Kc, Kh, Kw = weight.shape
+    W_out = int(np.floor((W + 2*padding[0] - dilation[0]*(Kw-1) - 1) / stride[0] + 1))
+    H_out = int(np.floor((H + 2*padding[1] - dilation[1]*(Kh-1) - 1) / stride[1] + 1))
+    result = np.zeros((N, C, H_out, W_out), dtype=np.float32)
+    _conv_forward_type2(np_input_pad,np_weight, np_bias, stride, result)
+    return torch.from_numpy(result).type_as(input)
+
 def conv_forward_type3(input, weight, bias, stride, padding, dilation, groups):
     np_input_pad = np.pad(input.data.cpu().numpy(), pad_width=((0,), (0,), (padding[0],), (padding[1],)), mode='constant', constant_values=0)
     np_weight = weight.data.cpu().numpy()
@@ -49,6 +63,20 @@ def _conv_forward_type1(input_pad, weight, bias, stride, result):
         for y in range(H_out):
           for x in range(W_out):
             for ti in range(Kc):
+              for ky in range(Kh):
+                for kx in range(Kw):
+                    result[n,to,y,x] += weight[to,ti,ky,kx] * input_pad[n,ti,(y*stride[1])+ky,(x*stride[0])+kx]
+            result[n,to,y,x] += bias[to]
+
+@jit(parallel=True)
+def _conv_forward_type2(input_pad, weight, bias, stride, result):
+    C, Kc, Kh, Kw = weight.shape
+    N, C, H_out, W_out = result.shape
+    for n in prange(N):
+      for to in prange(C):
+        for y in prange(H_out):
+          for x in prange(W_out):
+            for ti in prange(Kc):
               for ky in range(Kh):
                 for kx in range(Kw):
                     result[n,to,y,x] += weight[to,ti,ky,kx] * input_pad[n,ti,(y*stride[1])+ky,(x*stride[0])+kx]
