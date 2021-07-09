d55ec58ae2c7d10c0b09ce598f53d7bb04474f17
btxuyenhcmus <btxuyenhcmus@gmail.com>
2021-07-09 16:56:28 +0700

    [ADD] prelu parallel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src import detect_faces, show_bboxes, detect_faces_type1, detect_faces_type2,detect_faces_type3
 from PIL import Image
 img = Image.open('images/family.jpeg')
-bounding_boxes, landmarks = detect_faces_type3(img)
+bounding_boxes, landmarks = detect_faces(img)
 show_bboxes(img, bounding_boxes, landmarks)
